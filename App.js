17bba84f255606e3798a83538bb12d3215baf3ba
Valentin Barit <valentin.barit@gmail.com>
2018-09-19 23:19:29 +0200

    style: transform App to functionnal component

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import RootNavigation from './RootNavigation';
 
-export default class App extends Component {
-  render() {
-    return <RootNavigation />;
-  }
-}
+export default () => <RootNavigation />;
