06fe7ae4b9c2aa25863e222f8ba63d1c2a64da32
Valentin Barit <valentin.barit@gmail.com>
2019-04-09 23:10:19 +0200

    style: fix eslint error

--- src/ui/HomeButton/index.js
+++ src/ui/HomeButton/index.js
@@ -9,8 +9,8 @@ const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 5,
     backgroundColor: black,
+    borderRadius: 5,
     marginBottom: 10,
     marginTop: 10,
     width: (width * 2) / 3,
@@ -18,10 +18,5 @@ const styles = StyleSheet.create({
 });
 
 export default ({ onPress, title }) => (
-  <Button
-    title={toUpper(title)}
-    buttonStyle={styles.button}
-    fontSize={28}
-    onPress={onPress}
-  />
+  <Button title={toUpper(title)} buttonStyle={styles.button} fontSize={28} onPress={onPress} />
 );
